1ef3624aa08e5c319f7520e40db315f3cf2d31ae
harshavardhan-macha <172028181+harshavardhan-macha@users.noreply.github.com>
2025-05-29 15:08:12 +0530

    added the books array of objects

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let books = [
+    { id: 1, title: '1984', author: 'George Orwell' },
+    { id: 2, title: '1984', author: 'George Orwell' },
+    { id: 3, title: '1984', author: 'George Orwell' },
+];
+
 app.use(express.json());
 
 app.listen(port,() => {
